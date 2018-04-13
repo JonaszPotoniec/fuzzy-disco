65d118ae0a12f0573198aa4cdaf3831afe175c16
JonaszPotoniec <joni311099@gmail.com>
2018-04-13 14:20:58 +0200

    Started working on scheduler

--- dashboard/modules/wizyty/index.php
+++ dashboard/modules/wizyty/index.php
@@ -1,3 +1,16 @@
-<div>
-    gogogogoo
+<div id="container">
+    <div id="wizyty">
+        <ul>
+            <li>kk</li>
+            <li>kk</li>
+            <li>kk</li>
+            <li>kk</li>
+            <li>kk</li>
+            <li>kk</li>
+            <li>kk</li>
+            <li>kk</li>
+        </ul>
+    </div>
+    <input id="datepicker">
+    <script>$('#datepicker').datepicker();</script>
 </div>
