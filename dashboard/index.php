98a6cb0d6bb0987eaca77cb9d78bce3d1c5a0ce7
JonaszPotoniec <joni311099@gmail.com>
2018-04-14 14:21:35 +0200

    Created module with visits

--- dashboard/index.php
+++ dashboard/index.php
@@ -1,8 +1,10 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <link href="style.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/jquery-ui.min.css" rel="stylesheet">
     <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/jquery-ui.min.js"></script>
 	<link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i&amp;subset=latin-ext" rel="stylesheet">
 </head>
 <body>
--- dashboard/modules/wizyty/index.php
+++ dashboard/modules/wizyty/index.php
@@ -1,16 +1,37 @@
 <div id="container">
     <div id="wizyty">
         <ul>
-            <li>kk</li>
-            <li>kk</li>
-            <li>kk</li>
-            <li>kk</li>
-            <li>kk</li>
-            <li>kk</li>
-            <li>kk</li>
-            <li>kk</li>
+            <li>kk<button class="circleBtn" onclick="removeEvent($(this).parent())">X</button></li>
+            <li>kk<button class="circleBtn" onclick="removeEvent($(this).parent())">X</button></li>
+            <li>kk<button class="circleBtn" onclick="removeEvent($(this).parent())">X</button></li>
+            <li>kk<button class="circleBtn" onclick="removeEvent($(this).parent())">X</button></li>
+            <li>kk<button class="circleBtn" onclick="removeEvent($(this).parent())">X</button></li>
+            <li>kk<button class="circleBtn" onclick="removeEvent($(this).parent())">X</button></li>
+            <li>kk<button class="circleBtn" onclick="removeEvent($(this).parent())">X</button></li>
+            <li>kk<button class="circleBtn" onclick="removeEvent($(this).parent())">X</button></li>
         </ul>
     </div>
-    <input id="datepicker">
-    <script>$('#datepicker').datepicker();</script>
+    <div id="kalendarz">
+    </div>
+    <img id="kosz" src="/img/kosz.png">
+    <button id="nowaWizyta" class="circleBtn" onClick="loadModule('nowaWizyta')">+</button>
+    <script>
+        function removeEvent(obj){
+            if(confirm("Czy na pewno chcesz odwołać wizytę?")){
+                $(obj).remove();
+            }
+        }
+        $('#kalendarz').datepicker();
+        $("#wizyty ul li").each(function(){
+            $(this).draggable({ revert: true });
+        })
+        $("#kosz").droppable({
+            drop: function(event, ui){
+                removeEvent(ui.draggable)
+            },
+            classes: {
+                "ui-droppable-hover": "ui-state-hover, hover"
+            }
+        })
+    </script>
 </div>
