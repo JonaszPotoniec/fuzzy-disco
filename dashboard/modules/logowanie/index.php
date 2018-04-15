9254f83ee42dd9dd60bb749a9cdb41e290bf5b39
Kukulak1 <kukulak1@github.com>
2018-04-15 20:37:24 +0200

    First pack to register

--- dashboard/modules/logowanie/index.php
+++ dashboard/modules/logowanie/index.php
@@ -2,6 +2,30 @@
 
     session_start();
 
+    if(isset($_POST['email']))
+    {
+        //Udana walidacja? Załóżmy że tak!
+        $wszystko_OK=true;
+        
+        //Sprawdź poprawność loginu
+        $login = $_POST['nazwa-uzytkownika'];
+        
+        //Sprawdzenie dlugosci loginu
+        if ((strlen($login)<3) || (strlen($login)>20))
+        {
+            $wszystko_OK=false;
+            $_SESSION['e_login']="Login musi posiadać od 3 do 20 znaków";
+        }
+        
+        if($wszystko_OK==true)
+        {
+           //User dodany
+            echo "Udana walidacja!"; exit();
+            
+        }
+            
+    }
+
     if((isset($_SESSION['zalogowany']))&&($_SESSION['zalogowany']==true))
     {
         header('Location: ../../index.php');
@@ -30,10 +54,21 @@
     <input id="rejestracjaCheckbox" type="checkbox">
     <label for="rejestracjaCheckbox"><div  class="niceBtn">Zarejestruj się</div></label>
     <div id="rejestracja">
-        <form >
+        <form method="post">
             <fieldset>
                 <legend>Zarejestruj się</legend>
                 <input name="nazwa_uzytkownika" placeholder="Login" type="text"><br>
+                
+                <?php
+                
+                    if(isset($_SESSION['e_login']))
+                    {
+                        echo '<div class="error">'.$_SESSION['e_login'].'</div>';
+                        unset($_SESSION['e_login']);
+                    }
+                
+                ?>
+                
                 <input name="haslo1" placeholder="Hasło" type="password"><br>
                 <input name="haslo2" placeholder="Powtórz hasło" type="password"><br>
                 <input name="email" placeholder="email" type="email"><br>
@@ -46,8 +81,10 @@
                 <input name="kod" placeholder="Kod pocztowy" type="password"><br>
                 <input name="miasto" placeholder="Miasto" type="password"><br>
                 <label>
-                <input tyle="checkbox"><br> Akceptuje regulamin <br>
+                <input type="checkbox"><br> Akceptuje regulamin <br>
                 </label>
+                <div class="g-recaptcha" data-sitekey="6Lf-WlMUAAAAAHaj0HQ39dDufIv9vX0_EbUauNvS">
+                </div>
                 <button type="submit" class="niceBtn">Zarejestruj się</button>
             </fieldset>
         </form>
