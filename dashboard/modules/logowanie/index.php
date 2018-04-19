dbe52788c94339fbfad83844a0067249b4ff2eeb
JonaszPotoniec <joni311099@gmail.com>
2018-04-19 09:08:58 +0200

    Fixed freeze when opening login form on logged in user

--- dashboard/modules/logowanie/index.php
+++ dashboard/modules/logowanie/index.php
@@ -4,7 +4,7 @@
 
     if((isset($_SESSION['zalogowany']))&&($_SESSION['zalogowany']==true))
     {
-        header('Location: ../../index.php');
+        echo "<script>loadModule('wizyty')</script>";
         exit();
     }
 ?>
