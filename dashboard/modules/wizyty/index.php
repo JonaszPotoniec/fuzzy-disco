1c85f70d6b28e9e2c48ed35fb9cd25bfd20ce9d5
Jan Stefański <janstefanskite@gmail.com>
2018-04-20 00:40:46 +0200

    Made isActive thing happen
    Php gives me conniptions and suicidal thoughts. Never again.

--- dashboard/modules/wizyty/index.php
+++ dashboard/modules/wizyty/index.php
@@ -6,53 +6,44 @@
     ?>
     <div id="wizyty">
         <ul>
-        <?php
+            <?php
             require_once "../logowanie/connect.php";
-            
+
             $polaczenie = @new mysqli($host, $db_user, $db_password, $db_name);
-            if($polaczenie->connect_errno!=0)
-            {
+            if ($polaczenie->connect_errno != 0) {
                 throw new Exception(mysqli_connect_errno());
             }
-        
-            if($_SESSION['pacjent'] == 1){
-                if ($rezultat = @$polaczenie->query("SELECT * FROM wizyty WHERE idPacjent=".$_SESSION['userID'])){
-                    if($rezultat->num_rows > 0){
+
+            if ($_SESSION['pacjent'] == 1) {
+                if ($rezultat = @$polaczenie->query("SELECT * FROM wizyty WHERE isActive=1 AND idPacjent=" . $_SESSION['userID'])) {
+                    if ($rezultat->num_rows > 0) {
                         $wiersz = $rezultat->fetch_assoc();
-                        if($osoba = @$polaczenie->query('SELECT * FROM lekarze WHERE idLekarze='.$wiersz['idLekarz'])){
-                            $osoba = $osoba->fetch_assoc(); 
-                            echo "<li>".$wiersz['Data']." - ".$wiersz['Godzina']." - ".$osoba['Specjalizacja']." - ".$osoba['Nazwisko'].'<button class="circleBtn" onclick="removeEvent($(this).parent())">X</button></li>';
-                        }
-                        else
-                        {
+                        if ($osoba = @$polaczenie->query('SELECT * FROM lekarze WHERE idLekarze=' . $wiersz['idLekarz'])) {
+                            $osoba = $osoba->fetch_assoc();
+                            echo "<li>" . $wiersz['Data'] . " - " . $wiersz['Godzina'] . " - " . $osoba['Specjalizacja'] . " - " . $osoba['Nazwisko'] . '<button class="circleBtn" onclick="removeEvent($(this).parent())">X</button></li>';
+                        } else {
                             throw new Exception($polaczenie->error);
                         }
                     }
-                }
-                else
-                {
+                } else {
                     throw new Exception($polaczenie->error);
                 }
-            }else{
-                if ($rezultat = @$polaczenie->query("SELECT * FROM wizyty WHERE idLekarz=".$_SESSION['userID'])){
-                    if($rezultat->num_rows > 0){
+            } else {
+                if ($rezultat = @$polaczenie->query("SELECT * FROM wizyty WHERE idLekarz=" . $_SESSION['userID'])) {
+                    if ($rezultat->num_rows > 0) {
                         $wiersz = $rezultat->fetch_assoc();
-                        if($osoba = @$polaczenie->query("SELECT * FROM pacjenci WHERE idPacjenci=".$wiersz['idPacjent'])){
+                        if ($osoba = @$polaczenie->query("SELECT * FROM pacjenci WHERE idPacjenci=" . $wiersz['idPacjent'])) {
                             $osoba = $osoba->fetch_assoc();
-                            echo "<li>".$wiersz['Data']." - ".$wiersz['Godzina']." - ".$osoba['Imie']." ".$osoba['Nazwisko'].'<button class="circleBtn" onclick="removeEvent($(this).parent())">X</button></li>';
-                        }
-                        else
-                        {
+                            echo "<li>" . $wiersz['Data'] . " - " . $wiersz['Godzina'] . " - " . $osoba['Imie'] . " " . $osoba['Nazwisko'] . '<button class="circleBtn" onclick="removeEvent($(this).parent())">X</button></li>';
+                        } else {
                             throw new Exception($polaczenie->error);
                         }
                     }
-                }
-                else
-                {
+                } else {
                     throw new Exception($polaczenie->error);
                 }
             }
-        ?>
+            ?>
         </ul>
     </div>
     <div id="kalendarz">
@@ -62,27 +53,32 @@
         <button id="nowaWizyta" class="circleBtn" onClick="loadModule('nowaWizyta')">+</button>
     </div>
     <script>
-        function removeEvent(obj){
-            if(confirm("Czy na pewno chcesz odwołać wizytę?")){
+        function removeEvent(obj) {
+            if (confirm("Czy na pewno chcesz odwołać wizytę?")) {
                 $(obj).remove();
+                <?php
+                require_once "../logowanie/connect.php";
+                $osoba = @$polaczenie->query("UPDATE wizyty SET isActive=0 WHERE `idWizyty`=" . $wiersz['idWizyty'])
+                ?>
             }
         }
+
         $('#kalendarz').datepicker();
-        $("#wizyty ul li").each(function(){
-            $(this).draggable({ 
+        $("#wizyty ul li").each(function () {
+            $(this).draggable({
                 revert: true,
-                start: function( event, ui){
+                start: function (event, ui) {
                     $("#kosz").css("display", "block");
                     $("#nowaWizyta").css("display", "none");
                 },
-                stop: function( event, ui){
+                stop: function (event, ui) {
                     $("#kosz").css("display", "none");
                     $("#nowaWizyta").css("display", "block");
                 }
             });
         })
         $("#actionDiv").droppable({
-            drop: function(event, ui){
+            drop: function (event, ui) {
                 removeEvent(ui.draggable)
             },
             classes: {
